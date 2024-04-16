89c1e8d984deecd39c409a681abc1b545370f71d
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-04-16 06:43:42 +0300

    init nextui and prisma

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { NextUIProvider } from "@nextui-org/react";
+
+interface ProviderProps {
+    children: React.ReactNode
+};
+
+export default function Provider({children}: ProviderProps){
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    )
+}
