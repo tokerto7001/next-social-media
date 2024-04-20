0dbbd4a872320021190d31421c0e427012b6463d
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-04-20 13:54:53 +0300

    feat: add zod library to parse formdata values

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,3 +1,5 @@
 'use server';
 
-export async function createComment(){}
+export async function createComment(){
+    //TODO: Revalidate the post show page
+}
--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,24 @@
-'use server';
+"use server";
 
-export async function createTopic(){}
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lower case letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+ 
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description")
+  });
+
+  if(!result.success) console.log(result.error.flatten().fieldErrors);
+
+  //TODO: Revalidate the homepage after a topic is created
+}
