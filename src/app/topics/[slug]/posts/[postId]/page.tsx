1691668265f585b9e057f05bc68272e167d09ec5
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-04-28 22:48:01 +0300

    refactor: add Skeleton for inital loading screen

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,6 +1,7 @@
 import CommentCreateForm from "@/components/comments/comment-create-form";
 import CommentList from "@/components/comments/comment-list";
 import PostShow from "@/components/posts/post-show";
+import PostShowLoading from "@/components/posts/post-show-loading";
 import { fetchCommentsByPostId } from "@/db/queries/comments";
 import paths from "@/paths";
 import Link from "next/link";
@@ -19,7 +20,7 @@ export default async function PostShowPage({params}: PostShowPageProps) {
     return (
       <div className="space-y-3">
           <Link className="underline decoration-solid" href={paths.showTopic(slug)}>{'< '}Back to {slug}</Link>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<PostShowLoading />}>
             <PostShow 
             postId={postId}
             />
