d37ee37c283f3a06f6f23ea0e907d49c04810f92
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-04-28 22:56:55 +0300

    feat: add fetchTopPosts feature

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -1,7 +1,7 @@
 import type { Post } from '@prisma/client';
 import { db } from '..';
 
-export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug>>[number];
+export type PostWithData = Awaited<ReturnType<typeof fetchPostsByTopicSlug | typeof fetchTopPosts>>[number];
 
 export function fetchPostsByTopicSlug(slug: string) {
     return db.post.findMany({
@@ -11,5 +11,21 @@ export function fetchPostsByTopicSlug(slug: string) {
             user: { select: { name: true } },
             _count: { select: { comments: true } }
         }
+    })
+}
+
+export async function fetchTopPosts(){
+    return db.post.findMany({
+        orderBy: [{
+            comments: {
+                _count: 'desc'
+            }
+        }],
+        include: {
+            topic: { select: {slug: true} },
+            user: { select: { name: true, image: true } },
+            _count: { select: { comments: true } }
+        },
+        take: 5
     })
 }
